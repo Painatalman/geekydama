1262b7290c6e44e5f16aa13ae0104ce4e8f89144
Miguel Costa <maiquedi@gmail.com>
2016-10-29 09:52:47 +0100

    css modules and postcss

--- src/components/Ball.js
+++ src/components/Ball.js
@@ -1,8 +1,14 @@
 import Vue from 'vue/dist/vue.min.js';
 
+import styles from './Ball.css';
+
 // Define a new component called todo-item
 module.exports = function() {
   Vue.component('ball', {
-    template: '<svg class="dragon-ball" style="margin: auto; width: 50px; position: absolute: top: 36%;" viewBox = "0 0 100 100" version = "1.1"><circle cx = "50" cy = "50" r = "50" fill = "orange" stroke = "navy" stroke-width = "0"/><polygon fill="red" points="45,45 50,25 55,45 75,45 60,55 65,75 50,65 35,75 40,55 25,45 45,45" /></svg>'
+    template: `
+    <svg class="${styles['dragon-ball']}"  viewBox = "0 0 100 100" version = "1.1">
+      <circle cx = "50" cy = "50" r = "50" fill = "orange" stroke = "navy" stroke-width = "0"/>
+        <polygon fill="red" points="45,45 50,25 55,45 75,45 60,55 65,75 50,65 35,75 40,55 25,45 45,45" />
+    </svg>`
   });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,48 +7,54 @@ var WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080/',
-    'webpack/hot/only-dev-server',
+    // 'webpack-dev-server/client?http://localhost:8080/',
+    // 'webpack/hot/only-dev-server',
     path.resolve(__dirname, 'src/main.js')
   ],
-  output: {
+   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   devServer: {
-      contentBase: './dist'
+    contentBase: './dist'
   },
   configFile: './.eslintrc',
   devtool: 'source-map',
   plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }),
-      // new webpack.optimize.DedupePlugin(),
-      // new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.NoErrorsPlugin(),
-      new WebpackNotifierPlugin(),
-      new webpack.HotModuleReplacementPlugin()
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new WebpackNotifierPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-  preloaders: [
-    {
+    preloaders: [{
       test: /\.s(a|c)ss$/,
       loader: 'stylelint'
-    }
-  ],
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        include: [
-          path.resolve(__dirname, 'src')
-        ]
-      },{
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1!postcss-loader',
-        include: path.join(__dirname, 'src')
-    }
-    ]
-  }
+    }],
+     loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, 'src')
+      ]
+    }, {
+      test: /\.css$/,
+      loaders: [
+        'style-loader',
+        'css-loader?modules&importLoaders=1',
+        'postcss-loader'
+      ],
+      include: path.join(__dirname, 'src')
+    }] 
+  },
+  postcss: function() {
+    return [
+      require('postcss-simple-vars')({ /* ...options */ }),
+      require('autoprefixer')({ /* ...options */ })
+    ];
+  }
 };
