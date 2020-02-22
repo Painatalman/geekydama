218312e5b296257a4848923e6e9ffe7e5eb86fb7
Painatalman <maiquedi@gmail.com>
2020-02-22 21:24:14 +0000

    set up 404 router

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,13 @@ Vue.use(VueRouter);
 new Vue({
   el: "#app",
   router,
-  template: `<router-view></router-view>`
+  template: `<router-view></router-view>`,
+  created() {
+    if (sessionStorage.redirect) {
+      // https://stackoverflow.com/questions/47677220/vuejs-history-mode-with-github-gitlab-pages
+      const redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      this.$router.push(redirect);
+    }
+  }
 });
